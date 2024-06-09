781b4b39e2a2b1d540f42be3f30fa81d63c34c5a
anthonym-hiring <anthonym.hiring@protonmail.com>
2024-06-09 03:18:50 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+/*jslint es6:true*/
+
+const header = document.querySelector(".navbar")
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+
